4610cf183dba5c722275e602161388934132cc5c
egor8685 <kuzmin.el@edu.spbstu.ru>
2022-09-24 11:26:06 +0300

    feat: add calculator program

---
+++ 03_cpp/calculator/calculator.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main()
+{
+	char o;
+	double x,y;
+	while (true)
+	{
+		cout << "enter your exercice\n";
+		cin >> x >> o >> y;
+		if (o == '+')
+		{ 
+			cout << x + y << endl;
+		}
+		if (o == '-')
+        {
+            cout << x - y << endl;
+        }
+		if (o == '*')
+        {
+            cout << x * y << endl;
+        }
+		if (o == '/')
+        {
+			if (y == 0) {
+				cout << "Error" << endl;
+			}
+			else {
+				cout << x / y << endl;
+			}
+        }
+		if (o == '^')
+        {
+			cout << pow(x, y) << endl;
+        }
+	}
+	return 0;
+}
