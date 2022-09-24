3f3d8292f5b80b4117860bcc4a4097c3f3796760
egor8685 <kuzmin.el@edu.spbstu.ru>
2022-09-24 11:16:22 +0300

    feat: add hello programm

---
+++ 03_cpp/intro/hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+	string a = "";
+	cout << "What's your name?" << endl;
+	cin >> a;
+	cout << "Hello, " << a << "!" << endl;
+	system("PAUSE");
+	return 0;
+}
