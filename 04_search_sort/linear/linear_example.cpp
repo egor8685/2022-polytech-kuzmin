b2370730f77dadc468f284ff3941b42975cada3c
egor8685 <kuzmin.el@edu.spbstu.ru>
2022-11-11 12:37:39 +0300

    feat: add examle and function at linear

---
+++ 04_search_sort/linear/linear_example.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() int main() 
+{
+    int a;
+    int arr[] = {1, 2, 3, 3, 3};
+    cout << "Enter element:" << endl;
+    cin >> a;
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int result[] = { 1, 2, 3, 3, 3 };
+
+    int num = count(arr, n, a, result);
+    if (num == -1)
+    {
+        cout << "Element is not present in array" << endl;
+    }
+    else
+    {
+        cout << "There are " << num << " elements" << endl;
+        cout << "Their idexes are " << endl;
+        for (int s = 0; s < num; s++)
+            cout << result[s] << endl;
+    }
+    system("pause");
+    return 0;
+}
