69ce2d40aaff605814c1945cab1f9a6c0f548527
egor8685 <kuzmin.el@edu.spbstu.ru>
2022-11-21 18:31:14 +0300

    feat: add 4.2 lab

--- 04_search_sort/sort_cmp/fill_array_random.cpp
+++ 04_search_sort/sort_cmp/fill_array_random.cpp
@@ -1,12 +1,13 @@
-#include "oleg.h"
+#include <iostream>
+#include <random>
 
 void fill_array_random(int arr[], int n, int a, int b)
 {
-    random_device dev;
-    mt19937 rng(dev());
-    uniform_int_distribution<mt19937::result_type> dist(a,b);
+    std::random_device dev;
+    std::mt19937 rng(dev());
+    std::uniform_int_distribution<std::mt19937::result_type> dist(a,b);
     
-    for(int i=0; i<n; i++)
+    for(int i=0; i<n; ++i)
     {
         arr[i] = dist(rng);
     }
--- 04_search_sort/sort_cmp/quick_sort.cpp
+++ 04_search_sort/sort_cmp/quick_sort.cpp
@@ -1,4 +1,12 @@
-#include "oleg.h"
+#include <iostream>
+
+void swap(int arr[], int idx_a, int idx_b)
+{
+    int temp = arr[idx_a];
+    arr[idx_a] = arr[idx_b];
+    arr[idx_b] = temp; 
+}
+
 
 int partition(int arr[], int start, int end)
 {
@@ -6,27 +14,34 @@ int partition(int arr[], int start, int end)
     int pivot = arr[start];
 
     int count = 0;
-    for (int i = start + 1; i <= end; i++) {
+    
+    for (int i = start + 1; i <= end; i++) 
+    {
         if (arr[i] <= pivot)
             count++;
     }
 
     int pivotIndex = start + count;
+    
     swap(arr, pivotIndex, start);
 
     int i = start, j = end;
 
-    while (i < pivotIndex && j > pivotIndex) {
+    while (i < pivotIndex && j > pivotIndex) 
+    {
 
-        while (arr[i] <= pivot) {
+        while (arr[i] <= pivot) 
+        {
             i++;
         }
 
-        while (arr[j] > pivot) {
+        while (arr[j] > pivot) 
+        {
             j--;
         }
 
-        if (i < pivotIndex && j > pivotIndex) {
+        if (i < pivotIndex && j > pivotIndex) 
+        {
             swap(arr, i, j);
             i++;
             j--;
