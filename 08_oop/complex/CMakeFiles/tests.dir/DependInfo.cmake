1f62909618cdb1580611346fc81012482463b61f
egor8685 <kuzmie.el@edu.spbstu.ru>
2022-12-01 17:38:43 +0300

    feat: lab 8 done

---
+++ 08_oop/complex/CMakeFiles/tests.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/egor/2022-polytech-kuzmin/08_oop/complex/custom_complex.cpp" "/home/egor/2022-polytech-kuzmin/08_oop/complex/CMakeFiles/tests.dir/custom_complex.cpp.o"
+  "/home/egor/2022-polytech-kuzmin/08_oop/complex/test_complex.cpp" "/home/egor/2022-polytech-kuzmin/08_oop/complex/CMakeFiles/tests.dir/test_complex.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "_deps/doctest-src/doctest"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 08_oop/complex/CMakeFiles/tests.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tests.dir/custom_complex.cpp.o"
+  "CMakeFiles/tests.dir/test_complex.cpp.o"
+  "tests"
+  "tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
